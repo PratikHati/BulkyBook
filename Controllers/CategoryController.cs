b603564c4660df90a46b929b92dc9cb37676b2fa
Pratik Hati <pratikhati.work@gmail.com>
2023-04-13 23:25:32 +0530

    Migration for category added | Index view created

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using BulkyBook.Data;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _db.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
